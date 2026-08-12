59f594ad0f92335458064e791e2700dba5f40878
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2026-08-12 17:47:54 +0800

    refactor: change user_id name to player_id

--- fetch.py
+++ fetch.py
@@ -12,7 +12,7 @@ from utils import (
     iter_fetchable_account_ids_and_player_ids
 )
 
-def iter_current_registered_account_ids_and_user_ids_without_profiles(date_str: str):
+def iter_current_registered_account_ids_and_player_ids_without_profiles(date_str: str):
     supabase = create_supabase_client()
 
     max_records_per_page = 1000
@@ -40,30 +40,33 @@ def iter_current_registered_account_ids_and_user_ids_without_profiles(date_str: 
         else:
             stop = True
 
-    for account_id, user_id in iter_fetchable_account_ids_and_player_ids():
+    for account_id, player_id in iter_fetchable_account_ids_and_player_ids():
         if account_id in existing_account_ids:
             continue
 
-        yield account_id, user_id
+        yield account_id, player_id
 
-supabase = create_supabase_client()
+def fetch_all_profiles():
+    date_str = str(datetime.now(ZoneInfo("Asia/Jakarta")).date())
+    account_player_id_tuple_list = [x for x in iter_current_registered_account_ids_and_player_ids_without_profiles(date_str)]
+    print(f"Total:", len(account_player_id_tuple_list))
 
-date_str = str(datetime.now(ZoneInfo("Asia/Jakarta")).date())
-account_user_id_tuple_list = [x for x in iter_current_registered_account_ids_and_user_ids_without_profiles(date_str)]
-print(f"Total:", len(account_user_id_tuple_list))
+    for account_id, player_id in (pbar := tqdm(account_player_id_tuple_list)):
+        pbar.set_description(f"Processing {account_id}")
+        profile = fetch_profile_from_gdbrowser_colon_with_account_id(account_id)
+        if int(profile["accountID"]) != account_id and player_id is not None:
+            player_id = player_id
+            profile = fetch_profile_from_gdbrowser_colon_with_player_id(player_id)
 
-for account_id, user_id in (pbar := tqdm(account_user_id_tuple_list)):
-    pbar.set_description(f"Processing {account_id}")
-    profile = fetch_profile_from_gdbrowser_colon_with_account_id(account_id)
-    if int(profile["accountID"]) != account_id and user_id is not None:
-        player_id = user_id
-        profile = fetch_profile_from_gdbrowser_colon_with_player_id(player_id)
-
-    if int(profile["accountID"]) != account_id:
-        print(f"Warning: {account_id} is skipped because the profile has different account_id.")
-    else:
-        insert_profile_into_supabase(date_str, profile)
+        if int(profile["accountID"]) != account_id:
+            print(f"Warning: {account_id} is skipped because the profile has different account_id.")
+        else:
+            insert_profile_into_supabase(date_str, profile)
     
-    time.sleep(1)
+        time.sleep(1)
 
-print("Done!")
+    return date_str
+
+if __name__ == "__main__":
+    fetch_all_profiles()
+    print("Done!")
